b49469da95384eca4ab4ed96e8781bc8b9058bda
Maram Abu Murad <maramabumurad97@gmail.com>
2021-04-26 22:29:48 +0300

    lab assignment

---
+++ js/javas.js
@@ -0,0 +1,54 @@
+'use strict'
+let userName =  prompt('Hey There, Tell Me What\'s Your Name?:)');
+alert('Welcome ' + userName);
+
+
+
+let x = prompt('Do I love desserts? (yes or no)').toLowerCase();
+if (x == 'yes')
+{
+    alert('yes That\'s right');}
+    else {
+        alert('You missed this.')
+    }
+
+
+let y = prompt('Do I watch Anime?').toLowerCase();
+if (y == 'yes')
+{
+    alert('yes That\'s right');}
+    else {
+        alert('You missed this.')
+    }
+
+
+
+let z = prompt('Am I a sleepy Person ').toLowerCase();
+if (z == 'no')
+{
+    alert('That\'s right');}
+    else {
+        alert('You missed this.')
+    }
+
+
+
+let a = prompt('Do I enjoy Coding?').toLowerCase();
+if (a == 'yes')
+{
+    alert('yes That\'s right');}
+    else {
+        alert('You missed this too.')
+    }
+
+
+
+let b = prompt('Am I raising a pet?').toLowerCase();
+if (b == 'no')
+{
+    alert('That\'s right');}
+    else {
+        alert('You missed this too.')
+    }
+
+alert('Well Well, Not Bad Hope We Know Each Other Better Next Time, ' + userName);

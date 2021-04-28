90b1b7080c9dcebd11afc5c00e51aa299b28e66a
Maram Abu Murad <maramabumurad97@gmail.com>
2021-04-28 15:56:47 +0300

    correctingIssues

--- js/javas.js
+++ js/javas.js
@@ -6,8 +6,8 @@ let x = prompt('Do I love desserts? (yes or no)').toLowerCase();
 if (x == 'yes' || x == 'y')
 { 
     alert('yes That\'s right');
-    console.log("true");
-score ++; }
+    
+score++; }
      if (x == 'no' || x == 'n' )
         alert('You missed this.')
     
@@ -17,8 +17,8 @@ let y = prompt('Do I watch Anime?').toLowerCase();
 if (y == 'yes' || y == 'y')
 {
     alert('yes That\'s right');
-console.log("true");
-score ++; }
+
+score++; }
 else if (y == 'no' || y == 'n' )
         alert('You missed this.')
 
@@ -27,8 +27,8 @@ let z = prompt('Am I a sleepy Person ').toLowerCase();
 if (z == 'no' || z == 'n' )
 {
     alert('That\'s right');
-console.log("true");
-score ++;}
+
+score++; }
 else if  (z == 'yes' || z == 'y')
         alert('You missed this.')
     
@@ -37,8 +37,8 @@ let a = prompt('Do I enjoy Coding?').toLowerCase();
 if  (a == 'yes' || a == 'y')
 {
     alert('yes That\'s right');
-console.log("true");
-score ++;}
+
+score++; }
 else if (a == 'no' || a == 'n' )
         alert('You missed this too.')
     
@@ -47,8 +47,7 @@ let b = prompt('Am I raising a pet?').toLowerCase();
 if (b == 'no' || b == 'n' )
 {
     alert('That\'s right');
-console.log("true");
-score ++;}
+score++; }
 else if  (b == 'yes' || b == 'y')
         alert('You missed this too.')
     
@@ -56,44 +55,44 @@ else if  (b == 'yes' || b == 'y')
 alert('Well Well, Not Bad Hope We Know Each Other Better Next Time, ' + userName);
 
 
-let c = prompt('And now I\'m gonna Ask you to guess my favourite number, from 1 - 10 , you only have four attempts! ');
+let c = Number( prompt('And now I\'m gonna Ask you to guess my favourite number, from 1 - 10 , you only have four attempts! '));
 
-for (i = 1 ; i <= 4 ;i++ )
+for ( let i = 1 ; i <= 3 ;i++ )
 {
     if ( c < 3 ){
-        c = prompt('Too Low, try Another');
+        c =Number( prompt('Too Low, Try Another'));
     }
-    else if ( c > 3 )
+     if ( c > 3 ) 
     {
-        c = prompt ('Too High,Try Another');
+        c =  Number(prompt ('Too High,Try Another'));
     }
-   else if ( c == 3 ) {
-        alert('You Geussed it Right!');
-        console.log("true");
-        score ++;
+    if ( c === 3 ) {
+        alert('You Guessed it Right!');
+        score++;
+        break;
     }
     }
 
 
 let dessert = ['cheesecake' , 'pancake' , 'waffles' , 'cake' ,'brownies'  ];
-let m = prompt('Now can you Gess which kind of dessets I prefer;I\'ll make it easier for you and tell you it\'s not arabian desserts? ');
-let flag = true;
+let m = prompt('Now can you Geuss which kind of dessets I prefer;I\'ll make it easier for you and tell you it\'s not arabian desserts? ');
+let flag = false;
 for ( let n = 1 ; n < 6 ; n++ ) {
 for ( let l = 0 ; l < dessert.length ; l++) {
  
  
     if ( m === dessert[l] ) {
  alert('You got it RIGHT.');
- score ++;
+ score++;
  flag = true;
  
 break;
 }
 
 } 
-if ( flag === true ){
-    break;
-}
+// if ( flag === true ){
+//     break;
+// }
  if (flag === false ); {
     m=prompt('Guess again !!');
       
